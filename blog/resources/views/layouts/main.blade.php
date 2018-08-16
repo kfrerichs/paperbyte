bd053587e5169690b6ed5f9d6d0d52101b566de7
sbroja <sandrabroja@gmail.com>
2018-08-16 14:31:15 +0200

    Merge Aktualisierung

--- blog/resources/views/layouts/main.blade.php
+++ blog/resources/views/layouts/main.blade.php
@@ -41,29 +41,12 @@
     @yield('content')
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
-    <script>
-        CKEDITOR.replace( 'messageArea')
-    </script>
-
-    <script>                                        // Löschen bestätigen
-      function ConfirmDelete()
-      {
-      var confirmation = confirm("Soll dieser Eintrag wirklich gelöscht werden?");
-      if (confirmation)
-        return true;
-      else
-        return false;
-      }
-    </script>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
   </body>
 </html>
