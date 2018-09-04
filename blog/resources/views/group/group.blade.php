4308afa63612e8fb96b6027641ec43fe45715ca1
sbroja <sandrabroja@gmail.com>
2018-09-04 19:17:50 +0200

    minor changes2

--- blog/resources/views/group/group.blade.php
+++ blog/resources/views/group/group.blade.php
@@ -6,29 +6,7 @@
         <div class="col-md-12 col-md-offset-2">
             <div class="panel panel-default">
                 <h1>Gruppe</h1>
-                @foreach($roles as $role)
-                    <div>
-                        <table>
-                            <h4>{{$role->name}}</h4>
-                            <!-- <tr>
-                                <td>Spieler:</td>
-                                <td>{{$group->username}}</td>
-                            </tr>
-                            <tr>
-                                <td>Geschlecht:</td>
-                                <td>{{$group->gender}}</td>
-                            </tr>
-                            <tr>
-                                <td>Treffpunkt:</td>
-                                <td>{{$group->meetingpoint_id}}</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>{{$group->background}}</td>
-                            </tr>  -->
-                        </table>
-                    </div>
-                @endforeach
+                
             </div>
         </div>
     </div> 
